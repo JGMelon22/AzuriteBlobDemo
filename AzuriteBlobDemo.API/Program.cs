7fa9635699754ff02e69712bb20ea251bc1e3adb
JGMelon22 <jgvasconcellos22@gmail.com>
2026-05-08 22:30:00 -0300

    feat: allow cors for front-end implementation

--- AzuriteBlobDemo.API/Program.cs
+++ AzuriteBlobDemo.API/Program.cs
@@ -1,9 +1,22 @@
 using AzuriteBlobDemo.API.Endpoints;
 using AzuriteBlobDemo.API.Extensions;
 using AzuriteBlobDemo.Infrastructure.Options;
+using Microsoft.Net.Http.Headers;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Enable Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyPolicy",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+            .WithHeaders(HeaderNames.ContentType, "multipart/form-data")
+            .WithMethods("POST", "GET", "DELETE");
+        });
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -23,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference(options => { options.WithTitle("Azurite Blob API"); });
 }
+
+app.UseCors("MyPolicy");
 
 app.UseHttpsRedirection();
 
